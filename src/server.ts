d0665cbe8be38cf71d2cf3d4b05622b65a96711f
GuillermoCAB <guiga.benitez@gmail.com>
2023-08-03 20:56:08 -0300

    Add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
