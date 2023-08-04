5dc894b218dd35fe44bfd81dae17e4d9bb055bc5
GuillermoCAB <guiga.benitez@gmail.com>
2023-08-03 21:16:00 -0300

    Add routes to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import usersRoutes from "./routes/users";
+import scheduleRoutes from "./routes/schedules";
+
 dotenv.config();
 
 const app = express();
@@ -10,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
 });
+
+app.use("/users", usersRoutes);
+app.use("/schedules", scheduleRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
