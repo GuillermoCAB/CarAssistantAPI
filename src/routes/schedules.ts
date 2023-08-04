017537c00e374422ccebc7210bb9117407f15e96
GuillermoCAB <guiga.benitez@gmail.com>
2023-08-03 21:17:50 -0300

    Add Joi validations to schedule routes

--- src/routes/schedules.ts
+++ src/routes/schedules.ts
@@ -1,10 +1,29 @@
 import express from "express";
 import Schedule from "../models/Schedule";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import Joi from "@hapi/joi";
+import JoiDate from "@hapi/joi-date";
+
+const joi = Joi.extend(JoiDate);
+
+const scheduleSchema = joi.object({
+  date: joi.date().format("YYYY-MM-DD").required(),
+  hour: joi
+    .string()
+    .pattern(/^(?:2[0-3]|[01][0-9]):[0-5][0-9]$/)
+    .required(), // HH:MM format
+  vehicle: joi.array().items(joi.string()).required(),
+});
 
 const router = express.Router();
 
 router.post("/schedules", authMiddleware, async (req, res) => {
+  const { error } = scheduleSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
   const { date, hour, vehicle } = req.body;
 
   const existingSchedule = await Schedule.findOne({
@@ -25,6 +44,12 @@ router.post("/schedules", authMiddleware, async (req, res) => {
 });
 
 router.delete("/schedules", authMiddleware, async (req, res) => {
+  const { error } = scheduleSchema.validate(req.query);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
   const { date, hour } = req.query;
 
   const schedule = await Schedule.findOne({ user: req.user.id, date, hour });
